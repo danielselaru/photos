8f220e56c4e2ad366256715156886f7f23a08ea0
danielselaru <selarud@yahoo.com>
2025-03-30 01:53:04 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { api_link } from './Api_link'
+import { api_link_key } from './Api_link';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetch(api_link,{
+            method:"GET",
+            headers:{
+                Authorization:api_link_key
+            }
+        })
+            .then((response) => response.json())
+            .then((json) => setData(json.photos));
+
+    }, [])
+
+    const photos = data.map((e,i)=>{
+        if(i<=3){
+        return(
+            <div key={i}>
+                <img src={e.src.small} alt=''/>
+            </div>
+           )
+        }
+    })
+
+    return (
+        <div>{photos}</div>
+    )
 }
 
-export default App;
+export default App
