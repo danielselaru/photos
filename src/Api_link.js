5b8f058141d783d295494f6a18255ca0e2a0c715
danielselaru <selarud@yahoo.com>
2025-04-11 23:09:14 +0300

    aa

--- src/Api_link.js
+++ src/Api_link.js
@@ -1,3 +1,3 @@
 export const api_link_key ="CFr7hQD1PSbdMG0aSYVQXcgSDkYDYUvOcBAoIngnmKtqlG75nSz2PtAf"
 
-export const api_link = "https://api.pexels.com/v1/search?query=Ocean"
+export const api_link = "https://api.pexels.com/v1/search?query=Tigers"
--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import { api_link } from './Api_link'
 import { api_link_key } from './Api_link';
+import "./style.css"
 
 function App() {
 
     const [data, setData] = useState([])
+    const [img,setimg] = useState(false)
+
+    function extend_img(i) {
+         console.log(i)
+        // setimg(!img)
+        if(img && i==2){
+            // setimg((a)=>!a)
+             setimg(!img)
+        }
+    }
+    const styleimg = (img)?'big':'small'
 
     useEffect(() => {
         fetch(api_link,{
@@ -19,17 +31,17 @@ function App() {
     }, [])
 
     const photos = data.map((e,i)=>{
-        if(i<=3){
+     
         return(
-            <div key={i}>
-                <img src={e.src.small} alt=''/>
+            <div key={i} >
+                <img src={e.src.small} alt='' className={`images ${styleimg}`} onClick={()=>extend_img(i)}/>
             </div>
            )
         }
-    })
+    )
 
     return (
-        <div>{photos}</div>
+        <div className='detalii'>{photos}</div>
     )
 }
 
