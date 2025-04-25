3c4aee208222c55fd55b1fb00b7dbc261620ad0d
danielselaru <selarud@yahoo.com>
2025-04-25 22:31:52 +0300

    aa

--- src/Api_link.js
+++ src/Api_link.js
@@ -1,3 +1,2 @@
 export const api_link_key ="CFr7hQD1PSbdMG0aSYVQXcgSDkYDYUvOcBAoIngnmKtqlG75nSz2PtAf"
 
-export const api_link = "https://api.pexels.com/v1/search?query=Tigers"
--- src/App.js
+++ src/App.js
@@ -1,47 +1,55 @@
 import React, { useEffect, useState } from 'react'
-import { api_link } from './Api_link'
+// import { api_link } from './Api_link'
 import { api_link_key } from './Api_link';
 import "./style.css"
 
 function App() {
-
+   
     const [data, setData] = useState([])
-    const [img, setimg] = useState(false)
+    const [img, setimg] = useState(null)
+    const [type,settype] = useState('dog')
+    
+    const api_link =  `https://api.pexels.com/v1/search?query=${type}`
 
     function extend_img(i) {
-         console.log(img)
-        // setimg(!img)
-        if(img && i===2){
-            // setimg((a)=>!a)
-             setimg(!img)
-        }
+            setimg(i) 
     }
-    const styleimg = (img)?'big':'small'
+      
+    console.log(api_link)
 
     useEffect(() => {
-        fetch(api_link,{
-            method:"GET",
-            headers:{
-                Authorization:api_link_key
+        fetch(api_link, {
+            method: "GET",
+            headers: {
+                Authorization: api_link_key
             }
         })
             .then((response) => response.json())
             .then((json) => setData(json.photos));
 
-    }, [])
+    }, [api_link])
 
-    const photos = data.map((e,i)=>{
-     
-        return(
+    const photos = data.map((e, i) => {
+
+        return (
             <div key={i} >
-                <img src={e.src.small} alt='' className={`images ${styleimg}`} onClick={()=>extend_img(i)}/>
+                <img src={e.src.small} alt='' className={`images ${img === i ? 'big' : 'small'}`} onClick={() => extend_img(i)} key={i} />
             </div>
-           )
-        }
+        )
+    }
     )
 
     return (
-        <div className='detalii'>{photos}</div>
+        <div>
+            
+            <div className='buttons'>
+                <button onClick={() => settype('ocean')}>Ocean</button>
+                <button onClick={() => settype('tigers')}>Tigers</button>
+                <button onClick={() => settype('pears')}>Pears</button>
+                <button onClick={() => settype('back')}>Back</button>
+            </div>
+            <div className='detalii'>{photos}</div>
+        </div>
     )
 }
 
