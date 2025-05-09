23dffbe5b1eef706279c17a949fbade1ac99bbf5
danielselaru <selarud@yahoo.com>
2025-05-09 21:40:55 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import { api_link_key } from './Api_link';
 import "./style.css"
 
 function App() {
-   
+
     const [data, setData] = useState([])
     const [img, setimg] = useState(null)
-    const [type,settype] = useState('dog')
-    
+    const [type, settype] = useState('dog')
     const api_link =  `https://api.pexels.com/v1/search?query=${type}`
-
+    
     function extend_img(i) {
-            setimg(i) 
+        // setimg(i)
+        // const id = (i != null) ? setimg(null) : setimg(i);
+         setimg(id => id === i ? null : i);
     }
-      
+
     console.log(api_link)
 
     useEffect(() => {
@@ -33,7 +34,7 @@ function App() {
 
         return (
             <div key={i} >
-                <img src={e.src.small} alt='' className={`images ${img === i ? 'big' : 'small'}`} onClick={() => extend_img(i)} key={i} />
+                <img src={e.src.small} alt='' className={`images ${img === i ? 'big' : ''}`} onClick={() => extend_img(i)} key={i} />
             </div>
         )
     }
@@ -41,12 +42,11 @@ function App() {
 
     return (
         <div>
-            
+
             <div className='buttons'>
                 <button onClick={() => settype('ocean')}>Ocean</button>
                 <button onClick={() => settype('tigers')}>Tigers</button>
                 <button onClick={() => settype('pears')}>Pears</button>
-                <button onClick={() => settype('back')}>Back</button>
             </div>
             <div className='detalii'>{photos}</div>
         </div>
